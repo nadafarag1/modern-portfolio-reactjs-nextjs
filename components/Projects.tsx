26c9b013832a18e1da306d63ff364f4fb9eeba8f
nadafarag1 <20201329@tiba.edu.eg>
2025-04-27 01:09:53 +0300

    ContactMe

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,106 +1,221 @@
+// import React from 'react'
+// import { motion } from "framer-motion"
+// import Link from 'next/link'
+
+// type Props = {}
+
+// function Projects({}: Props) {
+
+//   const projects = [1, 2, 3, 4, 5];
+
+//   return (
+// <div className="h-screen relative flex overflow-hidden flex-col text-left
+//     max-w-full justify-evenly mx-auto items-center z-0 pr-3">
+
+//   <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
+//       text-2xl'>
+//         Projects
+//   </h3>
+
+//   <div className='container'>
+
+//   <ul className="cards">
+//     <li className="card">
+//       <div>
+//         <h3 className="card-title">Project 1</h3>
+//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt=""/>
+//         <div className="card-content">
+//           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+//         </div>
+//       </div>
+//       <div className="card-link-wrapper">
+//         <a href="" className="card-link">Read More</a>
+//       </div>
+//     </li>
+//     <li className="card">
+//       <div>
+//         <h3 className="card-title">Project 2</h3>
+//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt=""/>
+//         <div className="card-content">
+//           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
+//         </div>
+//       </div>
+//       <div className="card-link-wrapper">
+//         <a href="" className="card-link">Read More</a>
+//       </div>
+//     </li>
+//     <li className="card">
+//       <div>
+//         <h3 className="card-title">Project 3</h3>
+//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+//         <div className="card-content">
+//           <p>Phasellus ultrices lorem vel bibendum ultricies. In hendrerit nulla a ante dapibus pulvinar eu eget quam.</p>
+//         </div>
+//       </div>
+//       <div className="card-link-wrapper">
+//         <a href="" className="card-link">Read More</a>
+//       </div>
+//     </li>
+//     <li className="card">
+//       <div>
+//         <h3 className="card-title">Project 4</h3>
+//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+//         <div className="card-content">
+//           <p>Aenean posuere mauris quam, pellentesque auctor mi bibendum nec. Sed scelerisque lacus nisi, quis auctor lorem ornare vel.</p>
+//         </div>
+//       </div>
+//       <div className="card-link-wrapper">
+//         <a href="" className="card-link">Read More</a>
+//       </div>
+//     </li>
+//     <li className="card">
+//       <div>
+//         <h3 className="card-title">Project 5</h3>
+//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+//         <div className="card-content">
+//           <p>Vestibulum pharetra fringilla felis sit amet tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et arcu sit amet est consequat feugiat. Nam ut sapien pulvinar.</p>
+//         </div>
+//       </div>
+//       <div className="card-link-wrapper">
+//         <a href="" className="card-link">Read More</a>
+//       </div>
+//     </li>
+//     <li className="card">
+//       <div>
+//         <h3 className="card-title">Project 6</h3>
+//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+//         <div className="card-content">
+//           <p>Donec ut tincidunt nisl. Vivamus eget eros id elit feugiat mollis. Nam sed sem quis libero finibus tempor.</p>
+//         </div>
+//       </div>
+//       <div className="card-link-wrapper">
+//         <a href="" className="card-link">Read More</a>
+//       </div>
+//     </li>
+
+//   </ul>
+  
+// </div>
+// </div>
+//   )
+// }
+
+// export default Projects
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react'
 import { motion } from "framer-motion"
 import Link from 'next/link'
-import Image from 'next/image'
-import cicdPic from '../public/images/cicdPic.jpg'
-import ecommercePic from '../public/images/ecommercePic.jpg'
-import djangoRestPic from '../public/images/djangoRestPic.jpg'
-import mlopsPic from '../public/images/mlopsPic.jpg'
-import airbyteAirflowPic from '../public/images/airbyteAirflowPic.jpg'
-import portfolioPic from '../public/images/portfolioPic.jpg'
 
 type Props = {}
 
 function Projects({}: Props) {
 
-  const projects = {
-    1: {name:"Multi-branch CI-CD Pipeline", picture: cicdPic, description:"A Multi-branch CI-CD Pipeline Using Jenkins, Docker, AWS, Maven To Deploy an Odoo ERP custom module & a simple Java Maven web app.", link:"https://github.com/ahmedfarag9/CI-CD-Pipeline-Jenkins-Odoo-Project", preview: ''},
-    2: {name:"E-Commerce MERN App", picture: ecommercePic, description:"Full E-commerce MERN STACK Web-app, deployed on AWS EKS, implemented with Github Actions CI-CD pipeline, using Docker and Kubernetes.", link:"https://github.com/ahmedfarag9/ecommerce-webapp-mern-stack", preview: 'https://e-commerce-app-test.netlify.app/'},
-    3: {name:"Django REST-API Project", picture: djangoRestPic, description:"Built with Python 3.10, Django REST Framework and Docker. Configured with unit testing and deployed on Back4App container cloud hosting.", link:"https://github.com/ahmedfarag9/django-rest-framework-api-project", preview: ''},
-    4: {name:"MLOPS CICD Housepricing", picture: mlopsPic, description:"A MLOPS CI-CD imlementaion of a Linear Regression ML Model that predicts house pricing. With a simple UI & API endpoint for testing. Dockerized & packaged by pickle. Deployed on Heroku using GitHub Actions.", link:"https://github.com/ahmedfarag9/mlops-cicd-bostonhousepricing", preview: 'https://mlopscicdbostonhousepricing-ahmedfaraga12.b4a.run/'},
-    5: {name:"Airbyte Airflow Intergration", picture: airbyteAirflowPic, description:"Api integration using Airbyte-Airflow Mega Docker Cluster. Airbyte handles the Api connection & Airflow does the orchestration.", link:"https://github.com/ahmedfarag9/airbyte-airflow-api-intergration", preview: ''},
-    6: {name:"Modern Portfolio Website", picture: portfolioPic, description:"The one you're reading now. Built with REACT.JS & NEXT.JS Using (JavaScript, TypeScript, Framer Motion, Tailwind CSS). Automatic deployment on Github Pages using Github Actions.", link:"https://github.com/ahmedfarag9/modern-portfolio-reactjs-nextjs", preview:''},
-  }
+  
 
   return (
 <div className="h-screen relative flex overflow-hidden text-left flex-col
     max-w-full justify-evenly mx-auto items-center z-0 pr-3">
 
-  <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-400
+  <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
       text-2xl'>
         Projects
   </h3>
 
   <div className='container mt-24 mb-5'>
 
-  <ul className="cards">
-    {Object.entries(projects).map(([key, project]) =>  (
-      <li key={key} className="card">
-        <div>
-          <h3 className="card-title">{project.name}</h3>
+  <ul className="cards flex scroll scroll-snap">
+    <li className="card flex">
+      <div>
+        <h3 className="card-title">Project 1</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" width={927} height={455} decoding='async' loading='lazy' alt=""/>
+        <div className="card-content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 2</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
+    {/* <li className="card">
+      <div>
+        <h3 className="card-title">Project 3</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Phasellus ultrices lorem vel bibendum ultricies. In hendrerit nulla a ante dapibus pulvinar eu eget quam.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 4</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Aenean posuere mauris quam, pellentesque auctor mi bibendum nec. Sed scelerisque lacus nisi, quis auctor lorem ornare vel.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 5</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Vestibulum pharetra fringilla felis sit amet tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et arcu sit amet est consequat feugiat. Nam ut sapien pulvinar.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li>
+    <li className="card">
+      <div>
+        <h3 className="card-title">Project 6</h3>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
+        <div className="card-content">
+          <p>Donec ut tincidunt nisl. Vivamus eget eros id elit feugiat mollis. Nam sed sem quis libero finibus tempor.</p>
+        </div>
+      </div>
+      <div className="card-link-wrapper">
+        <a href="" className="card-link">Read More</a>
+      </div>
+    </li> */}
 
-          <div className='card-zoom'>
-                <Image
-                className='card-zoom-project-image '
-                src={project.picture}
-                alt='user profile picture'
-                />
-              </div>
-            <div className="card-content">
-              <p>{project.description}</p>
-            </div>
-          </div>
-          <div className="card-link-wrapper">
-            <a
-              href={project.link}
-              className="card-link mb-5"
-              target="_blank"
-              rel="noopener noreferrer">
-                Github
-            </a>
-            {project.preview &&
-            <a
-              href={project.preview}
-              className="card-link-with-preview m-5"
-              target="_blank"
-              rel="noopener noreferrer">
-                Live Preview
-            </a>
-            }
-          </div>
-        </li>
-
-
-
-
-        // <li key={key} className="card">
-        //   <div>
-        //     <h3 className="card-title">{project.name}</h3>
-        //       <Image
-        //       className='relative rounded-full h-40 w-40 mx-auto object-contain '
-        //       src={project.picture}
-        //       alt='user profile picture'
-        //       />
-        //     <div className="card-content">
-        //       <p>{project.description}</p>
-        //     </div>
-        //   </div>
-        //   <div className="card-link-wrapper">
-        //     <a
-        //       href={project.link}
-        //       className="card-link"
-        //       target="_blank"
-        //       rel="noopener noreferrer">
-        //         Read More
-        //     </a>
-        //   </div>
-        // </li>  
-      ))}
-
-    </ul>
-    
-    </div>
-  </div>
+  </ul>
+  
+</div>
+</div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import About from '../components/About'
 import WorkExperience from '../components/WorkExperience'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
+import ContactMe from '../components/ContactMe'
 
 
 
@@ -75,7 +76,9 @@ export default function Home() {
 
 
     { /* Contact Me */}
-
+      <section id='contact' className='snap-start'>
+        <ContactMe />
+      </section>
 
 
     </div>
