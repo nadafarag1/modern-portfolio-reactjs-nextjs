b5a486299dfbe38e65485d7632915039d7dda6ae
nadafarag1 <20201329@tiba.edu.eg>
2025-10-18 16:28:19 +0300

    chore: update dependencies to latest versions for next, react, and react-dom

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
 import { motion } from 'framer-motion'
+import Link from 'next/link'
+
 
 
 type Props = {}
 
-function Header({}: Props) {
+export default function Header({}: Props) {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl-items-center'>
-
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
         <motion.div
 		initial={{
 			x: -500,
@@ -45,7 +46,6 @@ function Header({}: Props) {
 
 
 
-		<a href="#contact">
 		<motion.div
 			initial={{
 				x: 500,
@@ -63,17 +63,21 @@ function Header({}: Props) {
 				
 
 		 className='flex flex-row items-center text-gray-300 cursor-pointer'>
-			<SocialIcon 
-			className='cursor-pointer'
-			network='email'
-			fgColor='gray'
-			bgColor='transparent'
-			/>
-			<p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
-				Get in Touch</p>
-		</motion.div>
-		</a>
-		
+				<SocialIcon
+                url='#contact'
+                className='cursor-pointer'
+                network='email' 
+                fgColor='gray'
+                bgColor='transparent'
+                />
+
+                <Link href='#contact'>
+
+                    <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+                        Get In Touch    
+                    </p>
+                </Link>
+            </motion.div>
 
 
 
@@ -82,4 +86,3 @@ function Header({}: Props) {
   )
 }
 
-export default Header
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,122 +1,3 @@
-// import React from 'react'
-// import { motion } from "framer-motion"
-// import Link from 'next/link'
-
-// type Props = {}
-
-// function Projects({}: Props) {
-
-//   const projects = [1, 2, 3, 4, 5];
-
-//   return (
-// <div className="h-screen relative flex overflow-hidden flex-col text-left
-//     max-w-full justify-evenly mx-auto items-center z-0 pr-3">
-
-//   <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
-//       text-2xl'>
-//         Projects
-//   </h3>
-
-//   <div className='container'>
-
-//   <ul className="cards">
-//     <li className="card">
-//       <div>
-//         <h3 className="card-title">Project 1</h3>
-//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt=""/>
-//         <div className="card-content">
-//           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-//         </div>
-//       </div>
-//       <div className="card-link-wrapper">
-//         <a href="" className="card-link">Read More</a>
-//       </div>
-//     </li>
-//     <li className="card">
-//       <div>
-//         <h3 className="card-title">Project 2</h3>
-//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt=""/>
-//         <div className="card-content">
-//           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
-//         </div>
-//       </div>
-//       <div className="card-link-wrapper">
-//         <a href="" className="card-link">Read More</a>
-//       </div>
-//     </li>
-//     <li className="card">
-//       <div>
-//         <h3 className="card-title">Project 3</h3>
-//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-//         <div className="card-content">
-//           <p>Phasellus ultrices lorem vel bibendum ultricies. In hendrerit nulla a ante dapibus pulvinar eu eget quam.</p>
-//         </div>
-//       </div>
-//       <div className="card-link-wrapper">
-//         <a href="" className="card-link">Read More</a>
-//       </div>
-//     </li>
-//     <li className="card">
-//       <div>
-//         <h3 className="card-title">Project 4</h3>
-//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-//         <div className="card-content">
-//           <p>Aenean posuere mauris quam, pellentesque auctor mi bibendum nec. Sed scelerisque lacus nisi, quis auctor lorem ornare vel.</p>
-//         </div>
-//       </div>
-//       <div className="card-link-wrapper">
-//         <a href="" className="card-link">Read More</a>
-//       </div>
-//     </li>
-//     <li className="card">
-//       <div>
-//         <h3 className="card-title">Project 5</h3>
-//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-//         <div className="card-content">
-//           <p>Vestibulum pharetra fringilla felis sit amet tempor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et arcu sit amet est consequat feugiat. Nam ut sapien pulvinar.</p>
-//         </div>
-//       </div>
-//       <div className="card-link-wrapper">
-//         <a href="" className="card-link">Read More</a>
-//       </div>
-//     </li>
-//     <li className="card">
-//       <div>
-//         <h3 className="card-title">Project 6</h3>
-//         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-//         <div className="card-content">
-//           <p>Donec ut tincidunt nisl. Vivamus eget eros id elit feugiat mollis. Nam sed sem quis libero finibus tempor.</p>
-//         </div>
-//       </div>
-//       <div className="card-link-wrapper">
-//         <a href="" className="card-link">Read More</a>
-//       </div>
-//     </li>
-
-//   </ul>
-  
-// </div>
-// </div>
-//   )
-// }
-
-// export default Projects
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import React from 'react'
 import { motion } from "framer-motion"
 import Link from 'next/link'
@@ -125,10 +6,10 @@ type Props = {}
 
 function Projects({}: Props) {
 
-  
+  const projects = [1, 2, 3, 4, 5];
 
   return (
-<div className="h-screen relative flex overflow-hidden text-left flex-col
+<div className="h-screen relative flex overflow-hidden flex-col text-left
     max-w-full justify-evenly mx-auto items-center z-0 pr-3">
 
   <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500
@@ -136,13 +17,13 @@ function Projects({}: Props) {
         Projects
   </h3>
 
-  <div className='container mt-24 mb-5'>
+  <div className='container'>
 
-  <ul className="cards flex scroll scroll-snap">
-    <li className="card flex">
+  <ul className="cards">
+    <li className="card">
       <div>
         <h3 className="card-title">Project 1</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" width={927} height={455} decoding='async' loading='lazy' alt=""/>
+        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt=""/>
         <div className="card-content">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
@@ -163,69 +44,7 @@ function Projects({}: Props) {
         <a href="" className="card-link">Read More</a>
       </div>
     </li>
-
-
     <li className="card">
-      <div>
-        <h3 className="card-title">Project 3</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
-
-
-
-    <li className="card">
-      <div>
-        <h3 className="card-title">Project 4</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
-
-
-
-
-    <li className="card">
-      <div>
-        <h3 className="card-title">Project 5</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
-
-
-
-
-
-    <li className="card">
-      <div>
-        <h3 className="card-title">Project 6</h3>
-        <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
-        <div className="card-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab repudiandae magnam harum natus fuga et repellat in maiores.</p>
-        </div>
-      </div>
-      <div className="card-link-wrapper">
-        <a href="" className="card-link">Read More</a>
-      </div>
-    </li>
-    {/* <li className="card">
       <div>
         <h3 className="card-title">Project 3</h3>
         <img src="https://cdn.pixabay.com/photo/2019/01/17/19/11/login-3938432_960_720.jpg" alt="" />
@@ -272,7 +91,7 @@ function Projects({}: Props) {
       <div className="card-link-wrapper">
         <a href="" className="card-link">Read More</a>
       </div>
-    </li> */}
+    </li>
 
   </ul>
   
