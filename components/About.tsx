7ebcf03e3247ac4d5584d4cf096f5d03af2c6438
nadafarag1 <20201329@tiba.edu.eg>
2025-10-18 17:13:24 +0300

    fix: correct className and update About section content

--- components/About.tsx
+++ components/About.tsx
@@ -10,7 +10,7 @@ function About() {
     transition={{ duration: 1 }}
     whileInView={{opacity: 1}}
 
-     className=' flex-col relative h-scflexreen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+     className=' h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'>
     <h3 className=' absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl
      About'>
         About
@@ -41,8 +41,12 @@ function About() {
         <div className='space-y-10 px-0 md:px-10'>
           <h4 className='text-4xl font'>Here is a <span className='underline decoration-[#f7AB0A]/50'>little</span> background</h4>
             <p className='text-base'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
+I             ’m a Frontend Developer and Digital Marketing Specialist with experience in both building and promoting digital products. 
+              On the technical side, I develop responsive and accessible web apps using React.js, Next.js, HTML5, CSS3, and JavaScript, with attention to UI/UX principles and Figma design workflows. 
+              I’m comfortable in Linux (Ubuntu) environments and use GitHub for version control and collaboration.
+              On the marketing side, I create and manage campaigns across Facebook, Instagram, LinkedIn, and TikTok, covering content creation (posts, reels, videos, designs), engagement strategies, paid ads, and market research. This combination of development and marketing expertise allows me to bridge the gap between technology and business delivering not just functional software, but products that engage users and drive real value.
+
+              Passionate about the intersection of commerce, technology, I thrive on bringing creative ideas to life while keeping business impact in focus.            </p>
 
         </div>
 
